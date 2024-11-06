7955905d9d30b109006dcdda9e5741051bb6b622
Frahane <frahanebio@gmail.com>
2024-11-06 20:22:50 +0100

    UI/UX Improved

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,4 +5,31 @@ $(document).ready(function() {
     $(this).toggleClass('scaled');
 });
     console.log("Document is ready!");
+
+    $(document).ready(function() {
+        // Navigation toggle functionality
+        $('.nav-toggle').click(function() {
+            $(this).toggleClass('active');
+            $('.nav-menu').slideToggle(300);
+        });
+    
+        // Close menu when clicking outside
+        $(document).click(function(event) {
+            if (!$(event.target).closest('.quick-nav').length) {
+                $('.nav-menu').slideUp(300);
+                $('.nav-toggle').removeClass('active');
+            }
+        });
+    
+        // Handle responsive behavior
+        $(window).resize(function() {
+            if ($(window).width() > 768) {
+                $('.nav-menu').show();
+            } else {
+                $('.nav-menu').hide();
+                $('.nav-toggle').removeClass('active');
+            }
+        });
+    });
+
 });
